2985f5c84ef8b274a2974977577f1be4c4184a2f
Alexey <alexey.lighthouse@gmail.com>
2018-05-22 04:06:15 +0300

    rebuilded menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,15 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Data</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                </div>
+                {{--<div class="panel-body">--}}
+                    {{--@if (session('status'))--}}
+                        {{--<div class="alert alert-success">--}}
+                            {{--{{ session('status') }}--}}
+                        {{--</div>--}}
+                    {{--@endif--}}
+                {{--</div>--}}
                 <div class="panel-body">
                     <a href='/home/products'>Products from store</a><br>
                     <a href='/home/brands'>Brands from store</a><br>
@@ -22,20 +22,25 @@
                     <a href='/home/supplier3'>Supplier3 Valadis (9264)</a><br>
                     <a href='/home/supplier4'>Supplier4 Wimood (441980)</a><br>
                 </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Update data</div>
+                <div class="panel-body">
+                    <a href='/import/supplier1'>check Supplier Real Solutions Haarlem</a><br>
+                    <a href='/import/supplier2'>check Supplier Api BV</a><br>
+                    <a href='/import/supplier3'>check Supplier Valadis</a><br>
+                    <a href='/import/supplier4'>check Supplier Wimood</a><br>
+                    <a href='/store/products'>check Stock</a><br>{{-- :TODO rename route--}}
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">Suppliers settings</div>
                 <div class="panel-body">
                     <a href='/home/show/supplier1'>show Supplier Real Solutions Haarlem</a><br>
                     <a href='/home/show/supplier2'>show Supplier Api BV</a><br>
                     <a href='/home/show/supplier3'>show Supplier Valadis</a><br>
                     <a href='/home/show/supplier4'>show Supplier Wimood</a><br>
-                </div>
-                <div class="panel-body">
-                    <a href='lsc.php?updateSupplier=367457,0'>check Supplier Api BV</a><br>
-                    <a href='lsc.php?updateSupplier=9183,0'>check Supplier Real Solutions Haarlem</a><br>
-                    <a href='lsc.php?updateSupplier=9264,0'>check Supplier Valadis</a><br>
-                    <a href='lsc.php?updateSupplier=441980,0'>check Supplier Wimood</a><br>
-                </div>
-                <div class="panel-body">
-                    <a href='/store/products'>check Stock</a><br>
                 </div>
             </div>
         </div>
